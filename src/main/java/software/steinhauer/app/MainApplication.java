8cfdb87a6927a86b514c0e83d55588c73f783573
Holger Steinhauer <holger+github@steinhauer.se>
2018-08-30 17:40:52 +0100

    Back to working layout and springsec extra

--- src/main/java/software/steinhauer/app/MainApplication.java
+++ src/main/java/software/steinhauer/app/MainApplication.java
@@ -21,9 +21,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class MainApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(MainApplication.class, args);
 	}
 }
