d6f7085f543a64674371ab6e6956719437268283
Holger Steinhauer <holger.steinhauer@esfcapital.com>
2018-08-30 10:32:00 +0100

    $STUFF

--- src/test/java/software/steinhauer/app/DemoApplicationTests.java
+++ src/test/java/software/steinhauer/app/DemoApplicationTests.java
@@ -1,3 +1,20 @@
+/*
+ * This file is part of Command Center.
+ *
+ * Command Center is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General  Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Command Center is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General  Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General  Public License
+ * along with Command Center.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package software.steinhauer.app;
 
 import org.junit.Test;
