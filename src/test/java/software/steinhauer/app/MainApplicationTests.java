216e3be24d0aa0c5098f1a26a9630547f49fcba2
Holger <holger+github@willige-entwickler.de>
2018-09-09 21:19:44 +0100

    Travis Integration (#3)
    * Adds config for TravisCI
    
    * Correcting JDK10 configuration for Travis
    
    * Adds master build status to README
    
    * "Fixing" test

--- src/test/java/software/steinhauer/app/MainApplicationTests.java
+++ src/test/java/software/steinhauer/app/MainApplicationTests.java
@@ -20,11 +20,13 @@ package software.steinhauer.app;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DemoApplicationTests {
+@ActiveProfiles("local")
+public class MainApplicationTests {
 
 	@Test
 	public void contextLoads() {
